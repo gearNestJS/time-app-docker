b2b103c6857322d0939c1b16bc4ba576e3ac6559
Valeriy Semenenko <gearmobile@gmail.com>
2024-10-22 23:11:50 +0300

    feat: :sparkles: add docker compose file

--- api/src/utils/mysqlPool.mjs
+++ api/src/utils/mysqlPool.mjs
@@ -2,11 +2,11 @@ import mysql from 'mysql2'
 
 const pool = mysql.createPool({
   connectionLimit: 100,
-  host: 'mysql',
-  port: '3306',
-  user: 'root',
-  password: 'password',
-  database: 'time_db',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_ROOT_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 })
 
 const CREATE_TIMES_TABLE_SQL = `CREATE TABLE IF NOT EXISTS times (
